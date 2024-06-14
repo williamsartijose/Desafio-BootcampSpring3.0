adf2dbee4e4677dd359c6842f4e2011c0da95817
william sarti <williamsartijose@hotmail.com>
2024-06-14 22:53:08 +0100

    Uploading the project

---
+++ src/test/java/com/wsj/bootcampspring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsj.bootcampspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
